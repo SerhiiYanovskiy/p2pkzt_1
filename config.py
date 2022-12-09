e4769c5a4d35461b480cf6f97a9f28eeedf76de4
serhii <https://github.com/SerhiiYanovskiy/socar.git>
2022-12-09 12:44:09 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,21 @@
+SPREADSHEET_ID = "1GCKQ-dEZYb44oiF-Nd43BVMHvXMBRxoVL17QjC1Hm_w"
+SPOT = ["USDT", "BTC", "BUSD", "BNB", "KZT", "ETH"]
+BANKS_KZT = ["KaspiBank", "HalykBank", "ForteBank", "CenterCreditBank", "JysanBank", "AltynBank", "EurasianBank",
+             "FreedomBank", "Nurbank", "HomeCreditKazakhstan", "BankRBK"]
+LIMITS_RUB = [1000, 5000, 10000, 20000, 30000, 50000, 100000, 200000, 300000, 500000, "Без указания среза"]
+LIMITS_KZT = [5000, 10000, 20000, 30000, 40000, 50000, 100000, 150000, 200000, 250000, 300000, 350000, 400000, 450000,
+              500000, "Без указания среза"]
+
+headers = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Content-Length": "123",
+    "content-type": "application/json",
+    "Host": "p2p.binance.com",
+    "Origin": "https://p2p.binance.com",
+    "Pragma": "no-cache",
+    "TE": "Trailers",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"}
